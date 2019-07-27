af537fb6f3452b0289ed73657008ed1562f45cd4
Marcus Foertsch <marcus@foertschemail.com>
2019-07-27 18:56:39 -0500

    Return HTML and JSON.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,21 @@ app.get('', (req, res) => {
     res.send('Hello Express');
 });
 
+app.get('/help', (req, res) => {
+    res.send('Help page');
+});
+
+app.get('/about', (req, res) => {
+    res.send('<h1>About</h1>');
+});
+
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: 'It\'s hot outside.',
+        location: 'United States'
+    });
+});
+
 app.listen(3000, () => {
    console.log('Server is up on port 3000');
 });
