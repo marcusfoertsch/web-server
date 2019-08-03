4ae30ecb6d1fafcdcd3115264e38ac123a116c83
Marcus Foertsch <marcus@foertschemail.com>
2019-08-03 12:08:13 -0500

    Fix destructuring error

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.get('/weather', (req, res) => {
         });
     }
 
-    geocode(address, (error, { latitude, longitude, location }) => {
+    geocode(address, (error, { latitude, longitude, location } = {}) => {
         if (error) {
             return res.send({
                 error: error
